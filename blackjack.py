87ec4d9902a1ccbb8dcb431b30eb5b4b26b9c015
PROTELLA44 <mcsdanika@gmail.com>
2022-08-12 22:54:02 +0200

    asd

--- blackjack.py
+++ blackjack.py
@@ -6,7 +6,10 @@ from tkinter import *
 # DEFAULT VALUES
 
 chips = 1000
-deck = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]*4
+deck = ["club_2","club_3","club_4","club_5","club_6","club_7","club_8","club_9","club_10","club_J","club_Q","club_K","club_A",
+        "spade_2","spade_3","spade_4","spade_5","spade_6","spade_7","spade_8","spade_9","spade_10","spade_J","spade_Q","spade_K","spade_A",
+        "diamond_2","diamond_3","diamond_4","diamond_5","diamond_6","diamond_7","diamond_8","diamond_9","diamond_10","diamond_J","diamond_Q","diamond_K","diamond_A",
+        "heart_2","heart_3","heart_4","heart_5","heart_6","heart_7","heart_8","heart_9","heart_10","heart_J","heart_Q","heart_K","heart_A"]
 deck_value = ["2","3","4","5","6","7","8","9","10","10","10","10","11"]*4
 playerChards = []
 dealerChards = []
